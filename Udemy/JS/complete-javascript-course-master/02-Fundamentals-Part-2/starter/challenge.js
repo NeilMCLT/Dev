33d7c73454eb17f6c890611986572dedccfe1dd4
Neil Miclat <neilmiclat@gmail.com>
2024-09-29 00:36:33 +0800

    I correct theChallenge 1 in part 2

--- Udemy/JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge.js
+++ Udemy/JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge.js
@@ -1,4 +1,4 @@
-//Challenge 1
+// //Challenge 1
 
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
@@ -7,13 +7,12 @@ let scoreKoalas = calcAverage(65, 54, 49);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
     if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
     } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win (${scoreKoalas} vs. ${scoreDolphins})`);
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
-        console.log(`No team wins`);
+        console.log(`No team wins...`);
     }
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
-
