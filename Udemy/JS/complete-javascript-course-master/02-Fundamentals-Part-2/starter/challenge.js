64d387a414820d63151a49d33eecc864660d555d
Neil Miclat <neilmiclat@gmail.com>
2024-09-30 19:41:33 +0800

    Done Doing Challenge 2

--- Udemy/JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge.js
+++ Udemy/JS/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge.js
@@ -17,3 +17,19 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 
 checkWinner(scoreDolphins, scoreKoalas);
 */
+
+//Challenge 2 
+
+const calcTip = function (bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+};
+
+let bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
