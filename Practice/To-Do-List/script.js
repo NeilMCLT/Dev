0b25b98c9910599ccc474af36a3fcc3d41102fea
Neil Miclat <neilmiclat@gmail.com>
2024-10-14 20:28:33 +0800

    Continue the Todo list

--- Practice/To-Do-List/script.js
+++ Practice/To-Do-List/script.js
@@ -4,9 +4,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
 
 // Functions
 function addTodo(event) {
@@ -20,7 +24,7 @@ function addTodo(event) {
 
     // This will create a LI inside the new div
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo)
 
@@ -38,4 +42,45 @@ function addTodo(event) {
 
     // This will append as a child of .todo-list
     todoList.appendChild(todoDiv);
+    todoInput.value = '';
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // This will delete the specific todo item
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        })
+    }
+
+    // This will complete the specific todo item
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'uncompleted':
+                break;
+
+        }
+    })
+
 }
